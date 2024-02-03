4606a8ee92b86e93212bb5a171a7e660add70145
DhanushkaTa <dhanushka999ta@gmail.com>
2024-02-03 09:51:32 +0530

    Create project strecture

---
+++ src/main/java/lk/ijse/test/controller/UserController.java
@@ -0,0 +1,21 @@
+package lk.ijse.test.controller;
+
+import lk.ijse.test.util.ResponseUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/user")
+@CrossOrigin
+@RequiredArgsConstructor
+public class UserController {
+
+    @GetMapping()
+    public ResponseUtil getTestApi(){
+        return new ResponseUtil(200,"Ok........",null);
+    }
+
+}
